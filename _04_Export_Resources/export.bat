42770cbd95467c6543f1fe12ee88330aeb2fe9fb
Unknown <aeon.williams@digipen.edu>
2019-10-23 16:45:30 -0700

    WIP auto_export_script
    Batch file works!!!! Now to do the rest...

--- _04_Export_Resources/export.bat
+++ _04_Export_Resources/export.bat
@@ -1,6 +1,2 @@
-::comment!
-"C:\Program Files\Unity\Editor\Unity.exe" -batchmode -logFile "C:\Users\aeon.williams\Documents\DragonPack\_04_Export_Resources\logfile.txt" -projectPath "C:\Users\aeon.williams\Documents\DragonPack\_04_Export_Resources\exporter" -exportPackage "\Assets\Scripts\New folder" "LPK.unitypackage"  -quit
-::-projectPath "C:\Users\aeon.williams\Documents\DragonPack\_04_Export_Resources\exporter" -exportPackage "C:\Users\aeon.williams\Documents\DragonPack\_04_Export_Resources\exporter\Assets\Scripts" "testPackage.unitypackage" -quit
-::-batchmode -quit -projectPath "C:\Users\aeon.williams\Documents\DragonPack\_04_Export_Resources\exporter" 
-::-exportPackage "C:\Users\aeon.williams\Documents\DragonPack\_04_Export_Resources\exporter\Assets\Scripts"
-::-serial E3-XXXX-XXXX-XXXX-XXXX-XXXX
+"C:\Program Files\Unity\Editor\Unity.exe" -batchmode -logFile "C:\Users\aeon.williams\Documents\DragonPack\_04_Export_Resources\logfile.txt" -projectPath "C:\Users\aeon.williams\Documents\DragonPack\_04_Export_Resources\exporter" -exportPackage "Assets" "../test.unitypackage"  -quit
+
