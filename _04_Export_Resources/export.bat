9908cea3af7536b3cb61f8d09b7cd040a995c9d8
Unknown <aeon.williams@digipen.edu>
2019-10-23 16:46:00 -0700

    i forgot to include my commnets oops

--- _04_Export_Resources/export.bat
+++ _04_Export_Resources/export.bat
@@ -1,2 +1,12 @@
 "C:\Program Files\Unity\Editor\Unity.exe" -batchmode -logFile "C:\Users\aeon.williams\Documents\DragonPack\_04_Export_Resources\logfile.txt" -projectPath "C:\Users\aeon.williams\Documents\DragonPack\_04_Export_Resources\exporter" -exportPackage "Assets" "../test.unitypackage"  -quit
 
+::open unity
+::create project
+::close unity
+::move copy of package files into project path
+::open unity
+::go to project
+::export packages
+::close unity
+::delete temp project
+:: (:
